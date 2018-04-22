eb9454f8f4c7bb561ece25590872d6e1e164abf9
Danqing Liu <git@danqing.io>
2018-04-22 17:21:04 +0800

    Add production build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+git checkout gh-pages
+rm -rf font *.css *.js *.html build.sh
+git checkout master src build.sh webpack.config.js package.json
+git reset HEAD
+npm run build-prod
+mv -fv docs/* ./
+rm -rf src docs build.sh webpack.config.js package.json
+git add -A
+git commit -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push origin gh-pages ; git checkout master
