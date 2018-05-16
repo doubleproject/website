ec2df3bfaa90fe13dc06179edf965617e41300ca
Danqing Liu <git@danqing.io>
2018-05-16 20:54:48 +0800

    Reorganize source files for better structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,54 +3,103 @@ const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const production = process.env.NODE_ENV === 'production';
 
 var plugins = [
-  new HtmlWebpackPlugin({
-    filename: 'index.html',
-    template: 'pug/index.pug',
-  }),
   new ExtractTextPlugin('[name].css'),
   new CopyWebpackPlugin([
     {from: 'img', to: 'img'},
   ]),
 ];
 var externals = {};
+var optimization = {};
+
+// This is since mini css extract plugin doesn't support hot reloading.
+var css = null;
 
 if (production) {
   plugins = plugins.concat([
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.MinChunkSizePlugin({
       minChunkSize: 51200,
     }),
-    new webpack.DefinePlugin({
-      'process.env': {'NODE_ENV': JSON.stringify('production')}
-    }),
     new webpack.LoaderOptionsPlugin({minimize: true}),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {warnings: false},
-      comments: false
-    }),
   ]);
+
+  css = [MiniCssExtractPlugin.loader, {
+    loader: 'css-loader',
+    options: {importLoaders: true},
+  }, {
+    loader: 'postcss-loader',
+    options: {
+      plugins: function() {
+        return [autoprefixer];
+      }
+    }
+  }, {
+    loader: 'sass-loader',
+    options: {outputStyle: 'compressed'}
+  }];
 
   externals = {
     react: 'React',
     'react-dom': 'ReactDOM',
   };
+
+  optimization ={
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {comments: false},
+          compress: {
+            warnings: false,
+            dead_code: true,
+          },
+        },
+      }),
+      new OptimizeCSSAssetsPlugin(),
+    ],
+  };
 } else {
   plugins = plugins.concat([
+    new ExtractTextPlugin('[name].css'),
     new webpack.LoaderOptionsPlugin({debug: true}),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
   ]);
+
+  css = ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: [{
+      loader: 'css-loader',
+      options: {importLoaders: true},
+    }, {
+      loader: 'postcss-loader',
+      options: {
+        plugins: function() {
+          return [autoprefixer];
+        }
+      }
+    }, {
+      loader: 'sass-loader',
+      options: {outputStyle: 'compressed'}
+    }],
+  });
 }
 
 module.exports = {
   entry: {
-    main: './js/main.js',
-    index: './js/index.js',
+    main: './shared/main.js',
+    index: './index.js',
+    'cli/index': './cli/index.js',
   },
 
   output: {
@@ -70,29 +119,19 @@ module.exports = {
       loader: 'json-loader'
     }, {
       test: /\.pug$/,
-      loader: 'pug-loader'
+      use: [
+        'file-loader?name=[path][name].html',
+        'extract-loader',
+        'html-loader',
+        'pug-html-loader',
+      ]
     }, {
       test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          options: {importLoaders: true},
-        }, {
-          loader: 'postcss-loader',
-          options: {
-            plugins: function() {
-              return [autoprefixer];
-            }
-          }
-        }, {
-          loader: 'sass-loader',
-          options: {outputStyle: 'compressed'}
-        }]
-      })
-    }]
+      use: css,
+    }],
   },
 
   plugins: plugins,
-  externals: externals
+  externals: externals,
+  optimization: optimization
 };
